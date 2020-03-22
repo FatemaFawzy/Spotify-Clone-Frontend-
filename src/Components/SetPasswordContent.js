86d64705c147378b1d6288c347d78cc35a75a2f1
alihalafawy <alihalafawy99@gmail.com>
2020-03-22 16:13:45 +0200

    SetPassword phase 1 modification.

--- src/Components/SetPasswordContent.js
+++ src/Components/SetPasswordContent.js
@@ -3,21 +3,31 @@ import './SetPasswordContent.css';
 
 const SetPasswordContent=()=>(
 
-<div className="SetPasswordContent">
+<div class="SetPasswordContent">
    <div className="container" >
         <h1 className="header">Set device password</h1>
         <p>To use Spotify on your devices, you need to set a separate password and log in with the username provided.</p>
    </div>
 
-   <div className="container" style={{paddingTop: "'30px", paddingBottom: "30px" ,paddingLeft: "19px",paddingRight: "19px", backgroundColor: "white"}}>
-     <div id="UsernameAndButton" className="UsernameAndButton">
-       <p>Your device username is: <strong>31zgyk3lu32eedylbcl7jc3qgr7y</strong></p> 
-       <button id="SendEmailToSetPassword" type="submit" className="ButtonGreen btn btn-success">Send email to set password</button>
+  <div className="container" style={{paddingTop: "30px", paddingBottom: "30px" ,paddingLeft: "19px",paddingRight: "19px", backgroundColor: "white"}}>
+  <form>
+    
+    <div className="form-group">
+    <label for="exampleInputPassword1">Enter your current password:</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
     </div>
-     <div id="EmailSent" className="d-none">
-         <p id="MessageAfterSend">An email featuring your device username and a link to set your password has been sent to alihalafawy99@gmail.com</p>
-     </div>
-   </div>
+   
+    <div className="form-group">
+    
+    <label for="exampleInputPassword2">Enter your new password:</label>
+    <input type="password" className="form-control" id="exampleInputPassword2"/>
+   
+    </div>
+
+  <button type="submit" className="ButtonGreen btn btn-success">Submit</button>
+ </form>
+   
+  </div>
 
 </div>
 
