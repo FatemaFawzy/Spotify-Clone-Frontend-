44ec4d74872587c4cc6a7cf48e117d01a70eebbd
mohammedgrey <mohammeddsaadd@yahoo.com>
2020-04-04 20:31:20 +0200

    Redux connection.

--- src/Containers/YourLibraryPages/YourLibraryPlaylist.js
+++ src/Containers/YourLibraryPages/YourLibraryPlaylist.js
@@ -3,38 +3,58 @@ import './YourLibraryPlaylist.css';
 import YourLibraryNavbar from "../../Components/YourLibraryComponents/YourLibraryNavbar";
 import GeneralItem from "../GenericComponenets/GeneralItem";
 import LikedSongsComponent from '../../Components/YourLibraryComponents/LikedSongsComponent';
-
+import {connect} from "react-redux";
 class YourLibraryPlaylist extends Component {
   constructor(props){
     super(props);
     this.state = {
-    playlistArray:[
-        {image:"https://i.scdn.co/image/ab67706f00000002aa93fe4e8c2d24fc62556cba",
-        name:"Mood Booster",
-        description:"Ride on dem swings",
-        id:"1"},
-        {image:"https://i.scdn.co/image/ab67706f0000000262822373d6fce52feea1501c",
-        name:"Pop PunkFaves",
-        description:"Your favorite punks.",
-        id:"2"},
-        {image:"https://i.pinimg.com/474x/75/28/e7/7528e70aef7cf03be7a5fb13a163f476.jpg",
-        name:"Today's Top Hits",
-        description:"Drake is on top of the Hot 100!",
-        id:"3"},
-        {image:"https://pl.scdn.co/images/pl/default/2d5e3dba500f5a4f87581eaf7e294089806b6ba9",
-        name:"Comfort Zone",
-        description:"Chilled songs that you are familiar with - no surprises!",
-        id:"4"},
-        {image:"https://i.scdn.co/image/ab67706f00000002b6b079ea1e8f5af584d7a897",
-        name:"Hip Hop Drive",
-        description:"Hey there homie..",
-        id:"5"}
-    ],
-    total:""
+    playlistArray:[],
+    total:"",
     }
   }
   componentDidMount() {
-    const playlists = this.state.playlistArray.map(item => {
+
+    //Make a request with this.props.userID
+    //assume this is the returned data
+
+    // const url = "https://jsonplaceholder.typicode.com/photos"; 
+    // fetch(url)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     this.setState({playlists:data.slice(0,10)})
+    //   })
+    //   .catch((error)=>{
+    //     console.log(error);
+    //   })
+
+    const playlistArray=
+    [
+      {image:"https://i.scdn.co/image/ab67706f00000002aa93fe4e8c2d24fc62556cba",
+      name:"Mood Booster",
+      description:"Ride on dem swings",
+      id:"1"},
+      {image:"https://i.scdn.co/image/ab67706f0000000262822373d6fce52feea1501c",
+      name:"Pop PunkFaves",
+      description:"Your favorite punks.",
+      id:"2"},
+      {image:"https://i.pinimg.com/474x/75/28/e7/7528e70aef7cf03be7a5fb13a163f476.jpg",
+      name:"Today's Top Hits",
+      description:"Drake is on top of the Hot 100!",
+      id:"3"},
+      {image:"https://pl.scdn.co/images/pl/default/2d5e3dba500f5a4f87581eaf7e294089806b6ba9",
+      name:"Comfort Zone",
+      description:"Chilled songs that you are familiar with - no surprises!",
+      id:"4"},
+      {image:"https://i.scdn.co/image/ab67706f00000002b6b079ea1e8f5af584d7a897",
+      name:"Hip Hop Drive",
+      description:"Hey there homie..",
+      id:"5"}
+  ];
+
+    
+    const playlists = playlistArray.map(item => {
       return (
         <GeneralItem
         image={item.image}
@@ -63,4 +83,12 @@ return (
 );
 }
 }
-export default YourLibraryPlaylist;
+
+const mapStateToProps = state => {
+
+  return {
+    userID:state.userID
+  };
+
+};
+export default connect(mapStateToProps)(YourLibraryPlaylist);
