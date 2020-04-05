14d45450b18b70523b35c86cb829fc1276adeefd
hoda257 <hodahossam257@gmail.com>
2020-04-05 17:30:19 +0200

    connected login and sign out to redux store

--- src/Store/actions.js
+++ src/Store/actions.js
@@ -4,3 +4,5 @@ export const SELECT_SONG = "SELECT_SONG";
 export const SELECT_PROFILE = "SELECT_PROFILE";
 export const SELECT_PLAYLIST = "SELECT_PLAYLIST";
 export const ON_SIGNUP = "ON_SIGNUP";
+export const ON_SIGNOUT = "ON_SIGNOUT";
+export const ON_SIGNIN = "ON_SIGNIN";
