e05afab6df74a1632f7192ad7acae9101444d2ba
alihalafawy <alihalafawy99@gmail.com>
2020-04-06 13:53:06 +0200

    email form validation

--- src/Containers/Login/login.js
+++ src/Containers/Login/login.js
@@ -5,69 +5,71 @@ import {connect} from "react-redux";
 import * as actionTypes from "../../Store/actions";
 
 const initialState = {
-  emailOrUsername: "",
-  emailOrUsernameError: "",
+  email: "",
+  emailError: "",
   password: "",
-  passwordError:""
+  passwordError:"",
+  incorrectData:""
 }
 
-
+const emailFormat = RegExp(
+  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+);
 class login extends Component{
   constructor(props){
     super(props);
  this.state = initialState;
   }
 
- validate = () => {
-   let email = this.state.emailOrUsername;
-   let password = this.state.password;
-   let emailOrUsernameError="";
-   let passwordError="";
-   let proceed=true;
-  if (email.length ===0 && password.length ===0)
-  {
-    emailOrUsernameError = "Please enter your Spotify username or email address.";
-     passwordError = "Please enter your password.";
-     this.setState({emailOrUsernameError, passwordError});
-     document.querySelector("#email-input").classList.add("red-border");
-     document.querySelector("#password-input").classList.add("red-border");
-     proceed = false;
-  }
-   else if (email.length === 0)
-   {
 
-     passwordError="";
-     emailOrUsernameError = "Please enter your Spotify username or email address.";
-     this.setState({emailOrUsernameError , passwordError});
-     document.querySelector("#email-input").classList.add("red-border");
-     document.querySelector("#password-input").classList.remove("red-border");
-     proceed = false;
-   }
-   else if (password.length === 0)
+ validateEmail = () => {
+  let email = this.state.email;
+  let emailError="";
+  let proceedemail=true;
+  if (email.length === 0)
    {
-     emailOrUsernameError="";
-     passwordError = "Please enter your password.";
-     this.setState({passwordError , emailOrUsernameError});
-     document.querySelector("#email-input").classList.remove("red-border");
-     document.querySelector("#password-input").classList.add("red-border");
-     proceed = false;
+     emailError = "Please enter your Spotify username or email address.";
+     this.setState({emailError});
+     document.querySelector("#email-input").classList.add("red-border");
+     proceedemail = false;
    }
    else {
-   emailOrUsernameError = "";
-   passwordError = "";
-   this.setState({email, emailOrUsernameError , password, passwordError});
-   document.querySelector("#email-input").classList.remove("red-border");
-   document.querySelector("#password-input").classList.remove("red-border");
-   proceed = true;
+    emailError = "";
+    this.setState({email, emailError});
+    document.querySelector("#email-input").classList.remove("red-border");
+    proceedemail = true;
+    }
+    return proceedemail;
+ }
+
+ validatePassword = () => {
+  let password = this.state.password;
+  let passwordError="";
+  let proceedpassword=true;
+  if (password.length === 0)
+   {
+     passwordError = "Please enter your password.";
+     this.setState({passwordError});
+     document.querySelector("#password-input").classList.add("red-border");
+     proceedpassword = false;
    }
-   return proceed;
- };
+   else {
+    passwordError = "";
+    this.setState({password, passwordError});
+    document.querySelector("#password-input").classList.remove("red-border");
+    proceedpassword = true;
+    }
+    return proceedpassword;
+ }
+
+
+
 
  handleEmail = event => {
-  let email = this.state.emailOrUsername;
+  let email = this.state.email;
   email = event.target.value;
-  this.state.emailOrUsername = email;
-  this.validate();
+  this.state.email = email;
+  this.validateEmail();
   
 
 };
@@ -75,29 +77,67 @@ handlePassword = event => {
   let pass = this.state.password;
   pass = event.target.value;
   this.state.password = pass;
-  this.validate();
+  this.validatePassword();
   
 
 };
 
  clickSubmit = event => {
-  let emailOrUsername = this.state.emailOrUsername;
+  let incorrect = this.state.incorrectData
+  let email = this.state.email;
+  let emailError = this.state.emailError;
   let password = this.state.password;
   event.preventDefault();
-  // console.log(emailOrUsername.length);
-  if(this.validate()){
+  // console.log(email.length);
+  this.validateEmail();
+  this.validatePassword();
+  if(!emailFormat.test(email) && email.length !== 0)
+  {
+    emailError="The email address is invalid."
+    document.querySelector("#email-input").classList.add("red-border");
+    this.setState({emailError})
+  }
+  else if(this.validateEmail() && this.validatePassword()){
      this.setState({initialState});
-    //  console.log(emailOrUsername.length);
+    //  console.log(email.length);
      var clr= document.querySelector("#login-form");
      clr.reset();
      let returnedData={
       id: 1,
       token:"9494985sjhbhd64c"
     }
-     this.props.onSignIn(returnedData.id,returnedData.token);
-     this.props.history.push('/account/');
-    
+      this.props.onSignIn(returnedData.id,returnedData.token);
+      // this.props.history.push('/account/');
+    //  const requestOptions = {
+    //     method:"POST",
+    //  }
+    //   var url = 'http://52.14.190.202:8000/api/users/login?email='+this.state.email+'&password='+this.state.password ; 
+    //   fetch(url,requestOptions)
+    //     .then((response) => {
+    //       return response.json();
+    //     })
+    //     .then((data) => {
+    //       console.log(data);
+    //       if (data == "Successful login")
+    //       {
+    //         this.props.history.push('/account/');
+    //       }
+    //       else if (data == "Either email or passwrod is incorrect")
+    //       {
+    //         document.querySelector(".incorrect").classList.remove("d-none");
+    //         incorrect = "Incorrect username or password.";
+    //       }
+    //       // else if (data == "Please go to your inbox and click the link to activate your Email.")
+    //       // {
+    //       //   document.querySelector(".incorrect").classList.remove("d-none");
+    //       //  this.state.incorrectData = "Please go to your inbox and click the link to activate your Email."; 
+    //       // }
+    //     })
+    //     .catch((error)=>{
+    //       console.log(error);
+    //     })
   }
+
 
  };
 
@@ -110,6 +150,7 @@ render() {
     <div className="container">
 
       <p className="first-line">To continue, log in to Spotify.</p>
+      <p className="incorrect d-none">{this.state.incorrectData}</p>
       <a id="facebook-button"  className=" buttons btn btn-block btn-facebook" href="#" role="button"><i className="fab fa-facebook logo" aria-hidden="true"></i>  Continue With Facebook</a>
 
         <div className="horizontal">
@@ -121,8 +162,8 @@ render() {
         <form id="login-form">
 
           <div className="form-group">
-            <input type="text" className="form-control " id="email-input" aria-describedby="emailHelp"  placeholder="Email adrress or username" onChange={this.handleEmail} name="emailOrUsername" />
-            <p  id="missing-email">{this.state.emailOrUsernameError}</p>           
+            <input type="text" className="form-control " id="email-input" aria-describedby="emailHelp"  placeholder="Email adrress or username" onChange={this.handleEmail} name="email" />
+            <p  id="missing-email">{this.state.emailError}</p>           
           </div>
 
           <div className="form-group">
@@ -162,7 +203,7 @@ render() {
 } 
 const mapDispatchToProps = dispatch => {
   return {
-    onSignIn : (userID,userToken) => dispatch ({type: actionTypes.ON_SIGNIN, payload: {id: userID , token: userToken} }),
+    onSignIn : (userToken) => dispatch ({type: actionTypes.ON_SIGNIN, payload: { token: userToken} }),
   };
 };
 
