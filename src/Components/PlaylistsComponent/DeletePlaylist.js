dc9feacfb853128cf1bbbc3c98992be391d5952d
hoda257 <hodahossam257@gmail.com>
2020-04-07 17:26:57 +0200

    added delete playlist request

--- src/Components/PlaylistsComponent/DeletePlaylist.js
+++ src/Components/PlaylistsComponent/DeletePlaylist.js
@@ -6,20 +6,26 @@ import {connect} from 'react-redux';
 
 
 class DeletePlaylist extends Component{
-
-handleDelete () {
-const url = BASEURL+ "/playlists?playlistName="+this.props.playlistName;    
-  console.log(url);
-  // const requestOptions = {
-  //   method: 'DELETE', 
-  //   headers: { 'x-auth': this.props.userToken }, 
-  // };    
-  //    fetch(url,requestOptions)
-  //     .then((res) => res.json())
-  //     .then((data) =>console.log(data))
-  //     .catch((err)=>console.log(err))
+  constructor(props){
+    super(props);
+  }
+  
+handleDelete (str) {
+  const url = BASEURL+ "playlists";    
+  const requestOptions = {
+    method: 'DELETE', 
+    headers: { 'x-auth':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZThhNzAxOTU0ZmU3NTJjMTQ5OGY3MjEiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTg2MTMxOTc0fQ.5CqQJG2E8n_1h8-_XC_tb1HbnVuIXstLQpTyjoWK-Dk',
+    'Content-Type': 'application/json' },
+    body: JSON.stringify({ playlistName: str})
+  }; 
+     fetch(url,requestOptions)
+      .then((res) => {res.json(); window.location.reload(true);})
+      .then((data) =>{ 
+        console.log(data);
+        
+      })
+      .catch((err)=>console.log(err))
 }
-
 
 render(){
   return(
@@ -31,14 +37,14 @@ render(){
 
             <button className="btn close-button" data-dismiss="modal"> <i className="fas fa-times"></i> </button>
         
-          <h2 className="d-flex justify-content-center mb-3" >Do You Really Want To Delete this playlist?</h2>
+          <h2 className="d-flex justify-content-center mb-3" >Do you really want to delete this playlist ?</h2>
           <div className="container ">
               <div className="row">
                 <div className="col-6 d-flex justify-content-end">
                 <button className="btn btn-dark border border-light rounded-pill text-center px-5 py-2 mt-3 font-weight-bold" data-dismiss="modal"> Cancel</button>
                 </div>
                 <div className="col-6 d-flex justify-content-start">
-                <a href="/webplayer/yourlibrary" className=" btn btn-success rounded-pill text-center px-5 py-2 mt-3 font-weight-bold" > Delete</a>
+                <a onClick={() => this.handleDelete(this.props.delete)} className=" btn btn-success rounded-pill text-center px-5 py-2 mt-3 font-weight-bold" data-dismiss="modal" > Delete</a>
           
             </div>
         </div>
@@ -52,9 +58,7 @@ render(){
 }
 const mapStateToProps = state =>{
   return{
-    playlistName: state.selectedPlaylistName,
     userToken: state.userToken
-
   };
 };
 
