d93c73c3fbf23b3b416e8c93978881f30c2abda7
FatemaFawzy <fatemafst2009@gmail.com>
2020-04-10 18:27:45 +0200

    RelatedArtists render test

--- src/UnitTesting/RelatedArtistsContent.test.js
+++ src/UnitTesting/RelatedArtistsContent.test.js
@@ -1,4 +1,4 @@
-import RelatedArtistsContent from '../Components/ArtistProfile/RelatedArtistsContent';
+import {RelatedArtistsContent} from '../Components/ArtistProfile/RelatedArtistsContent';
 import React from "react";
 import {shallow, configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
