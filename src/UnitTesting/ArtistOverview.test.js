527a8c62d2265746104ffc3a316e9558aff1b3d7
FatemaFawzy <fatemafst2009@gmail.com>
2020-04-10 23:04:36 +0200

    ArtistOverview unit testing

--- src/UnitTesting/ArtistOverview.test.js
+++ src/UnitTesting/ArtistOverview.test.js
@@ -1,21 +1,68 @@
 import React from 'react';
-import ArtistOverviewContent from '../Components/ArtistProfile/ArtistOverviewContent';
-import { shallow } from 'enzyme';
+import {ArtistOverviewContent} from '../Components/ArtistProfile/ArtistOverviewContent';
+import {shallow, configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({adapter: new Adapter()});
 
-describe("[UNIT] Testing the ArtistOverviewContent component", () => {
+describe("Testing ArtistOverviewContent redering", () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(<ArtistOverviewContent/>);
   });
 
-  describe("Testing ArtistOverviewContent redering", () => { 
-
-    it('displays the header of the page', () => {
-      expect(wrapper.find("#popular-h1").text()).toContain("Popular");    
-    });
-
+  it('displays the header of the page', () => {
+    expect(wrapper.find("#popular-h1").text()).toContain("Popular");    
   });
 
-  
 });
+
+describe("showMoreAlbums", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<ArtistOverviewContent/>);
+  });
+
+  it("should toggle between SHOW MORE and SHOW LESS on click", () => {
+    expect(wrapper.find("button#show-more-album-btn").text()).toContain("SHOW MORE");  
+    wrapper.find("button#show-more-album-btn").simulate('click');
+    expect(wrapper.find("button#show-more-album-btn").text()).toContain("SHOW LESS"); 
+    wrapper.find("button#show-more-album-btn").simulate('click');
+    expect(wrapper.find("button#show-more-album-btn").text()).toContain("SHOW MORE"); 
+  });
+
+  it("should toggle between up arrow icon and down arrow icon", () => {
+    expect(wrapper.find("i#albums-arrow.fas.fa-chevron-down"));  
+    wrapper.find("button#show-more-album-btn").simulate('click');
+    expect(wrapper.find("i#albums-arrow.fas.fa-chevron-up"));  
+    wrapper.find("button#show-more-album-btn").simulate('click');
+    expect(wrapper.find("i#albums-arrow.fas.fa-chevron-down")); 
+  });
+
+})
+
+describe("showMoreSingles", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<ArtistOverviewContent/>);
+  });
+
+  it("should toggle between SHOW MORE and SHOW LESS on click", () => {
+    expect(wrapper.find("button.btn.show-more").at(1).text()).toContain("SHOW MORE");  
+    wrapper.find("button.btn.show-more").at(1).simulate('click');
+    expect(wrapper.find("button.btn.show-more").at(1).text()).toContain("SHOW LESS"); 
+    wrapper.find("button.btn.show-more").at(1).simulate('click');
+    expect(wrapper.find("button.btn.show-more").at(1).text()).toContain("SHOW MORE"); 
+  });
+
+  it("should toggle between up arrow icon and down arrow icon", () => {
+    expect(wrapper.find("i#albums-arrow.fas.fa-chevron-down"));  
+    wrapper.find("button#show-more-album-btn").simulate('click');
+    expect(wrapper.find("i#albums-arrow.fas.fa-chevron-up"));  
+    wrapper.find("button#show-more-album-btn").simulate('click');
+    expect(wrapper.find("i#albums-arrow.fas.fa-chevron-down")); 
+  });
+
+})
