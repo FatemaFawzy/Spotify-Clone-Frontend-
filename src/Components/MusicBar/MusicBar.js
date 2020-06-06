52bb19e6d86b6023098c44d912de56bffbc1ccd3
mohammedgrey <mohammeddsaadd@yahoo.com>
2020-06-06 15:29:43 +0200

    Added ads related functions to redux

--- src/Components/MusicBar/MusicBar.js
+++ src/Components/MusicBar/MusicBar.js
@@ -29,6 +29,8 @@ class MusicBar extends Component {
     }
     this.forcedProgress=false;
     this.intervalUpdate = setInterval(this.onUpdate, 250);
+    this.songObject=Tracks[0];
+    this.EnteredAdsMode=false;
   }
 
   likeSong = e => {
@@ -145,13 +147,35 @@ class MusicBar extends Component {
         volumeIcon="fa-volume-up";
       }
 
-      if(!this.refs.player.loop)
-      {
+      
         if(this.refs.player.ended&&this.props.somethingIsPlaying)
         {
+          console.log("Ended")
+          this.props.onSongEnded();
+          if(!this.refs.player.loop)
+          {
           this.props.onPlayPause();
+          }
         }
-      }
+
+        if(this.props.adsModeOn)
+        {
+          if(!this.EnteredAdsMode)
+          {
+            // this.songObject=Tracks[1];
+            this.refs.player.load();
+            this.EnteredAdsMode=true;
+          }
+          
+          console.log(this.songObject)
+          if(this.refs.player.ended&&this.props.somethingIsPlaying)
+          {
+            this.props.onAdsEnded();
+            this.EnteredAdsMode=false;
+            // this.refs.player.load();
+          }
+        }
+      
     }
 
     if(this.props.somethingIsPlaying) {
@@ -231,7 +255,14 @@ class MusicBar extends Component {
                 <button className="middle-icons fas fa-random mr-2"></button>
                 <button className="middle-icons fas fa-step-backward"></button>
                 <button id="play-track-bar" className="play middle-icons far fa-play-circle mr-3 ml-3" onClick={this.playPause} ></button>
-                <button className="middle-icons fas fa-step-forward"></button>
+                <button 
+                className="middle-icons fas fa-step-forward"
+                onClick={()=>{
+                  if(this.refs.player) this.refs.player.load();
+                  this.songObject=Tracks[4];
+                  
+                }}
+                ></button>
                 <button 
                 style={{color:this.props.playOnRepeat?"#1db954":"rgb(179,179,179)"}} 
                 className="middle-icons fas fa-sync-alt ml-2"
@@ -286,7 +317,7 @@ class MusicBar extends Component {
         </div>
 
         <audio ref="player" loop={this.props.playOnRepeat}>
-          <source src="https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/055.mp3" />
+          <source src={this.props.adsModeOn?Tracks[32].songURL:this.songObject.songURL} />
           {/* <source src="https://www.computerhope.com/jargon/m/example.mp3" /> */}
          
         </audio>
@@ -303,7 +334,8 @@ const mapDispatchToProps = dispatch => {
     onPlayASong: (songID) => dispatch({type: actionTypes.PLAY_SONG, value:songID}),
     onPlayPause: ()=> dispatch({type: actionTypes.PLAY_PAUSE}),
     onToggleLoop: ()=> dispatch({type:actionTypes.TOGGLE_SONG_LOOP}),
-
+    onSongEnded: ()=> dispatch({type:actionTypes.INCREMENT_NUM_SONGS}),
+    onAdsEnded: ()=> dispatch({type:actionTypes.EXIT_ADS_MODE}),
   };
 
 };
@@ -313,6 +345,7 @@ const mapStateToProps = state =>{
     playingSongID: state.playingSongID,
     somethingIsPlaying: state.somethingIsPlaying,
     playOnRepeat: state.playOnRepeat,
+    adsModeOn: state.adsModeOn,
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(MusicBar)
