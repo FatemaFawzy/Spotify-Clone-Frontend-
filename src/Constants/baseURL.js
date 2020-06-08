22c315a422f33abac5532da34ffb459955884377
mariambaz <mariam.elbaz@hotmail.com>
2020-06-08 22:18:04 +0200

    Adjusted the second BASEURL.

--- src/Constants/baseURL.js
+++ src/Constants/baseURL.js
@@ -1,3 +1,3 @@
 // export const BASEURL = "http://18.223.133.157:8000/";
  //export const BASEURL = "http://spotifyclone.mocklab.io/";
-// export const BASEURL2 = "http://spotifyclonemock.mocklab.io";
+// export const BASEURL2 = "http://spotifyclonemock.mocklab.io/";
--- src/Containers/ArtistAccount/ArtistSongs.js
+++ src/Containers/ArtistAccount/ArtistSongs.js
@@ -42,7 +42,7 @@ export class ArtistSongs extends Component {
     headers:{'Content-Type':'authorizaion/json','x-auth':"x-auth"}
   }
 
-   const url1 ="http://spotifyclonemock.mocklab.io/artist/mysongs/id"; 
+   const url1 =BASEURL2+"artist/mysongs/id"; 
   fetch(url1,requestOptions1)
     .then((response) => {
       return response.json();
@@ -62,7 +62,7 @@ export class ArtistSongs extends Component {
       headers:{'Content-Type':'authorizaion/json','x-auth':"x-auth"}
     }
 
-    const url2 = "http://spotifyclonemock.mocklab.io/artist/mysongs/id"; 
+    const url2 = BASEURL2+"artist/mysongs/id"; 
     fetch(url2,requestOptions2)
       .then((response) => {
         return response.json();
