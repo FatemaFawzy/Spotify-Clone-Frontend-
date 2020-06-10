52d7eda743d9608a9a79185cea46cf67ded3006b
alihalafawy <alihalafawy99@gmail.com>
2020-06-10 16:13:19 +0200

    Artist webplayer test added

---
+++ src/UnitTesting/ArtistAccount/ArtistWebPlayer.test.js
@@ -0,0 +1,60 @@
+//NOTE THAT THE FILE NAME SHOUL BE MyComponent.test.js whare MyComponent IS THE NAME OF THE COMPONENT YOU WANNA TEST
+
+//BASIC INPORTS AND CONFIGURATIONS MADE EACH TIME
+import React from "react";
+import {shallow, configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({adapter: new Adapter()});
+
+//IMPORT THE COMPONENT YOU WANNA TEST
+import {ArtistWebPlayer} from "../../Containers/ArtistAccount/ArtistWebPlayer";
+
+ //CALL THE DESCRIBE FUNCTION 
+//IT TAKES TWO PARAMETERS 1ST: A TEXT DESCRIPTION
+//                        2ND: AN ARROW FUNCTION 
+
+
+
+
+describe("ArtistWebPlayer Render test", ()=>{
+
+  let wrapper;
+  beforeEach(() => {
+   wrapper = shallow(<ArtistWebPlayer /> ); 
+  });
+
+  
+  it("Should have 6 divs", ()=>{
+    const divs=wrapper.find("div");
+    expect(divs).toHaveLength(6);
+  })
+  
+
+  it("Should have 9 Routes", ()=>{ 
+    const headers=wrapper.find("Route");
+    expect(headers).toHaveLength(9);
+  })
+
+  it("Should have 1 switch", ()=>{
+    const switches=wrapper.find("Switch");
+    expect(switches).toHaveLength(1);
+  })
+
+})
+
+ 
+ 
+
+
+
+  
+
+
+
+   
+    
+  
+
+  
+
+
