8f0623ea90df7fd13f43d7995e8a49e28773c284
mohammedgrey <mohammeddsaadd@yahoo.com>
2020-06-10 22:20:55 +0200

    mocked the request of get followers and following

--- src/Components/NotificationHistory/NotificationHistory.js
+++ src/Components/NotificationHistory/NotificationHistory.js
@@ -6,26 +6,59 @@ import SearchBoxNotificationHist from './SearchBoxNotificationHist'
 import NotificationHistoryList from './NotificationHistoryList'
 import { Link } from "react-router-dom";
 
-
+import {BASEURL} from "../../Constants/baseURL";
+/** Class NotificationHistory
+ * @category NotificationHistory
+ * @extends Component
+ */
 class NotificationHistory extends Component {
   constructor() {
     super();
     this.state = {
+  /**Object of notification history - array of notifications
+   * @memberof NotificationHistory
+   * @type {Array<NotificationHistory>}
+   */
+
       notifications: [],
+
+    /**Object of notification history - searched word
+   * @memberof NotificationHistory
+   * @type {string}
+   */  
       searchfield:''
     };
   }
+  
+  /**Function for setting state on rendering and getting notification history
+   * @memberof NotificationHistory
+   * @func componentDidMount
+   */
+  
 
   componentDidMount() {
-    fetch("http://spotifyclone.mocklab.io/notifications/recent")
+
+    let theurl= BASEURL+ "notifications/recent"
+
+    fetch(theurl)
       .then(Response => Response.json())
       .then(users => this.setState({ notifications: users }));
   }
 
+      /**event, when the text change in the search box, the searchfield value changes
+   * @memberof NotificationHistory
+   * @func NotificationHistory
+   */
+     
   onSearchChange=(event)=>{
     this.setState({searchfield:event.target.value })  
       }
   render() {
+
+      /**Function for filtering the searched notificatoin and handeling the lowecase/upper case situation
+   * @memberof NotificationHistory
+   * @func NotificationHistory
+   */
         const filteredNotiHistory=this.state.notifications.filter(
           notifications=>{
             return notifications.username.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
